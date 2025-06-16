24d9cbeb6886ae3137f1ecd7b76e90c662a3ad98
Jasmine Fowler <146142173+minlcve@users.noreply.github.com>
2025-06-15 21:53:30 -0300

    Add files via upload

---
+++ Lab #4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PracticeDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(47, 195);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 0;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(118, 31);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(199, 31);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(12, 31);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(100, 20);
+            this.txtQuery.TabIndex = 3;
+            this.txtQuery.TextChanged += new System.EventHandler(this.txtQuery_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(215, 128);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.AllowDrop = true;
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(102, 226);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Lab #4/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PracticeDatabase
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection;
+        private MySqlDataAdapter adapter;
+        private DataSet dataSet;
+        private bool changesMade = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connStr = "server=localhost;user=root;password=Sunoo123!;database=testdb;";
+
+            try
+            {
+                connection = new MySqlConnection(connStr);
+                connection.Open();
+                lblStatus.Text = "Connected to MySQL database.";
+                btnSave.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Connection failed: " + ex.Message;
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string query = txtQuery.Text.Trim();
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                MessageBox.Show("Please enter a SQL SELECT query.");
+                return;
+            }
+
+            try
+            {
+                adapter = new MySqlDataAdapter(query, connection);
+                MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+
+                dataSet = new DataSet();
+                adapter.Fill(dataSet, "Results");
+                dataGridView1.DataSource = dataSet.Tables["Results"];
+
+                // Enable save on edit
+                dataSet.Tables["Results"].ColumnChanged += (s, args) =>
+                {
+                    changesMade = true;
+                    btnSave.Enabled = true;
+                };
+
+                lblStatus.Text = "Query loaded. You can now edit and save.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Query failed: " + ex.Message);
+            }
+        }
+
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection?.Close();
+                lblStatus.Text = "Disconnected from MySQL.";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error disconnecting: " + ex.Message;
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtQuery_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!changesMade)
+            {
+                MessageBox.Show("No changes to save.");
+                return;
+            }
+
+            try
+            {
+                adapter.Update(dataSet, "Results");
+                lblStatus.Text = "Changes saved successfully.";
+                btnSave.Enabled = false;
+                changesMade = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving changes: " + ex.Message);
+            }
+        }
+
+    }
+}
